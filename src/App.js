a62cc54118e1ccb100eb3c8d078c8875b7540ada
mariam1798 <Mariam.alneamah@gmail.com>
2024-04-02 16:03:35 +0100

    add preliminary components

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import HomePage from "./pages/HomePage";
 import UserPage from "./pages/UserPage";
 import UploadPage from "./pages/UploadPage";
 import JobsPage from "./pages/JobsPage";
+import RegisterPage from "./pages/RegisterPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/register" element={<RegisterPage />} />
         <Route path="/user" element={<UserPage />} />
         <Route path="user/upload" element={<UploadPage />} />
         <Route path="/jobs" element={<JobsPage />} />
---
+++ src/components/VideoUpload/VideoUpload.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function VideoUpload() {
+  return <div></div>;
+}
