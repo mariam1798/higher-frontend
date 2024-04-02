e52bda77dd854f546ad038e33e872ff3cdf103de
mariam1798 <Mariam.alneamah@gmail.com>
2024-04-02 15:45:24 +0100

    create basic pages routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import UserPage from "./pages/UserPage";
+import UploadPage from "./pages/UploadPage";
+import JobsPage from "./pages/JobsPage";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/user" element={<UserPage />} />
+        <Route path="user/upload" element={<UploadPage />} />
+        <Route path="/jobs" element={<JobsPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
