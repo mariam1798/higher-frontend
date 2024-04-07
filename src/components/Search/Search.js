66921db9614d84f84eb7486a4272f9f5cc47e500
mariam1798 <Mariam.alneamah@gmail.com>
2024-04-07 17:52:16 +0100

    create upload modal

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,9 +1,72 @@
 import "./Search.scss";
 import { useNavigate } from "react-router-dom";
 import user from "../../assets/icons/user.svg";
+import UploadModal from "../UploadModal/UploadModal";
+import axios from "axios";
+import { useState } from "react";
 
 export default function Search() {
   const navigate = useNavigate();
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [message, setMessage] = useState("");
+
+  const handleOpenModal = () => setModalIsOpen(true);
+
+  const handleCloseModal = () => setModalIsOpen(false);
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [formData, setFormData] = useState({
+    title: "",
+    description: "",
+  });
+
+  const handleChange = (e) => {
+    if (e.target.name === "file") {
+      setSelectedFile(e.target.files[0]); // Directly use the file object
+    } else {
+      setFormData({ ...formData, [e.target.name]: e.target.value });
+    }
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    if (!formData.title || !formData.description || !selectedFile) {
+      alert("You must fill in all the form fields");
+      return;
+    }
+    const uploadData = new FormData();
+    uploadData.append("file", selectedFile);
+    uploadData.append("title", formData.title);
+    uploadData.append("description", formData.description);
+    const token = localStorage.getItem("authToken");
+    try {
+      const response = await axios.post(
+        `${process.env.REACT_APP_API_BASE_URL}/users/videos`,
+        uploadData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      if (response.status === 200) {
+        setMessage("Highered successfully!");
+      } else {
+        setMessage("Upload failed!");
+      }
+
+      setTimeout(() => {
+        handleCloseModal();
+        navigate("/user");
+      }, 5000);
+      event.target.reset();
+    } catch (error) {
+      console.error("Error:", error);
+      alert("Upload error!");
+    }
+  };
 
   return (
     <section className="search">
@@ -15,12 +78,7 @@ export default function Search() {
             name="search"
           ></input>
         </div>
-        <button
-          onClick={() => {
-            navigate("/user/upload");
-          }}
-          className="search__button"
-        >
+        <button onClick={handleOpenModal} className="search__button">
           UPLOAD
         </button>
         <img
@@ -29,6 +87,14 @@ export default function Search() {
           className="search__avatar search__avatar--tablet"
         />
       </div>
+      <UploadModal
+        handleChange={handleChange}
+        handleSubmit={handleSubmit}
+        handleOpenModal={handleOpenModal}
+        modalIsOpen={modalIsOpen}
+        handleCloseModal={handleCloseModal}
+        message={message}
+      />
     </section>
   );
 }
---
+++ src/components/UploadModal/UploadModal.js
@@ -0,0 +1,90 @@
+import React, { useState } from "react";
+import Modal from "react-modal";
+import "./UploadModal.scss";
+import { Link } from "react-router-dom";
+import higher from "../../assets/icons/higherwhite.svg";
+
+export default function UploadModal({
+  modalIsOpen,
+  handleCloseModal,
+  handleChange,
+  handleSubmit,
+  message,
+}) {
+  return (
+    <Modal
+      isOpen={modalIsOpen}
+      ariaHideApp={false}
+      contentLabel="onRequestClose Example"
+      onRequestClose={handleCloseModal}
+      className="modal"
+      overlayClassName="Overlay"
+    >
+      <div className="modal__icon">
+        <img
+          onClick={handleCloseModal}
+          src=""
+          alt=""
+          className="modal__close"
+        />
+      </div>
+      <div className="modal__container">
+        <h2 className="modal__title">Upload to</h2>
+        <div className="modal__wrap">
+          <img className="modal__logo" src={higher} alt="higher" />
+        </div>
+        <form className="modal__form" onSubmit={handleSubmit}>
+          <div className="modal__group">
+            <label className="modal__label" htmlFor="title">
+              Title
+            </label>
+            <input
+              className="modal__input"
+              type="text"
+              name="title"
+              id="title"
+              onChange={handleChange}
+            />
+          </div>
+          <div className="modal__group">
+            <label className="modal__label" htmlFor="description">
+              Description
+            </label>
+            <input
+              className="modal__input"
+              type="text"
+              name="description"
+              id="description"
+              onChange={handleChange}
+            />
+          </div>
+          <div className="modal__buttons">
+            <input
+              type="file"
+              onChange={handleChange}
+              name="file"
+              style={{ display: "none" }}
+              id="fileInput"
+            />
+            <label htmlFor="fileInput" className="modal__upload">
+              Upload
+            </label>
+            <button className="modal__button">Submit</button>
+            {message && <p>{message}</p>}
+
+            <Link
+              onClick={handleCloseModal}
+              className="modal__navigate"
+              to="/user"
+            >
+              My profile
+            </Link>
+          </div>
+        </form>
+        <button onClick={handleCloseModal} className="modal__button">
+          Cancel
+        </button>
+      </div>
+    </Modal>
+  );
+}
